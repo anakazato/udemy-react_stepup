a344fbd4881b93fc9d1f3076fb647519d8a05600
anakazato <aki.nakazato.office@gmail.com>
2025-12-17 23:38:22 +0900

    ネストされたページ遷移

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import { Home } from './Home';
 import { Page1 } from './Page1';
 import { Page2 } from './Page2';
+import { Page1DetailA } from './Page1DetailA';
+import { Page1DetailB } from './Page1DetailB';
 
 export default function App() {
   return (
@@ -15,11 +17,22 @@ export default function App() {
         <Link to="/page2">Page2</Link>
       </div>
       <Switch>
-        <Route exact path="/">
+      <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
+        <Route path="/page1" render={() => (
+          <Switch>
+            <Route exact path="/page1">
+              <Page1 />
+            </Route>
+            <Route path="/page1/detailA">
+              <Page1DetailA />
+            </Route>
+            <Route path="/page1/detailB">
+              <Page1DetailB />
+            </Route>
+          </Switch>
+        ) }>
         </Route>
         <Route path="/page2">
           <Page2 />
